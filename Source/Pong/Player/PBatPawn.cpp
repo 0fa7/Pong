016e95acacb6c2b386cc0f45fd1ed9d20f0ccd25
Aaron J <7063015+0fa7@users.noreply.github.com>
2023-12-29 01:37:06 -0500

    Project start

---
+++ Source/Pong/Player/PBatPawn.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PBatPawn.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APBatPawn::APBatPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BatMesh = CreateDefaultSubobject<UStaticMeshComponent>("Bat Mesh");
+	BatMesh->SetupAttachment(RootComponent);
+	BatMesh->SetRelativeScale3D(RelativeBatScale);
+	BatMesh->SetRelativeLocation(RelativeBatLocation);
+	
+	GameCamera = CreateDefaultSubobject<UCameraComponent>("Game Camera");
+	GameCamera->SetupAttachment(RootComponent);
+	GameCamera->SetWorldLocation(WorldCameraLocation);
+	
+	ProjectileSpawnPlayer1 = CreateDefaultSubobject<USceneComponent>("Projectile Spawn Player 1");
+	ProjectileSpawnPlayer1->SetupAttachment(BatMesh);
+	SpawnLocation1 = -1.f * RelativeBatLocation;
+	SpawnLocation1.Y += ProjectileSpawnOffset;
+	ProjectileSpawnPlayer1->SetRelativeLocation(SpawnLocation1);
+	
+	ProjectileSpawnPlayer2 = CreateDefaultSubobject<USceneComponent>("Projectile Spawn Player 2");
+	ProjectileSpawnPlayer2->SetupAttachment(BatMesh);
+	SpawnLocation2 = -1.f * RelativeBatLocation;
+	SpawnLocation2.Y -= ProjectileSpawnOffset;
+	ProjectileSpawnPlayer2->SetRelativeLocation(SpawnLocation2);
+	
+}
+
+// Called when the game starts or when spawned
+void APBatPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APBatPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APBatPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Pong/Player/PBatPawn.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PBatPawn.generated.h"
+
+class UStaticMeshComponent;
+class UCameraComponent;
+
+
+UCLASS()
+class PONG_API APBatPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APBatPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> BatMesh;
+	
+	UPROPERTY(EditAnywhere)
+	FVector RelativeBatScale = {.1f, .1f, 1.f};
+	
+	UPROPERTY(EditAnywhere)
+	FVector RelativeBatLocation = {0.f, 0.f,-50.f};
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UCameraComponent> GameCamera;
+
+	UPROPERTY(EditAnywhere)
+	FVector WorldCameraLocation = {0.f, 0.f,0.f};
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USceneComponent> ProjectileSpawnPlayer1;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USceneComponent> ProjectileSpawnPlayer2;
+
+	UPROPERTY(EditAnywhere)
+	float ProjectileSpawnOffset = 300.f;
+
+	UPROPERTY(EditAnywhere)
+	FVector SpawnLocation1;
+
+	UPROPERTY(EditAnywhere)
+	FVector SpawnLocation2;
+};
